a7d52e3a8e7c52f95258c39bdc67fd562fb8c799
smoleo <leons1352@gmail.com>
2021-08-21 20:16:55 +0200

    test

--- app/src/main/java/com/example/whattoeat/MainActivity.java
+++ app/src/main/java/com/example/whattoeat/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //hallo leon
+    //hallo noah
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
